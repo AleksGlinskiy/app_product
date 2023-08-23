2d5c29b155e74c4bb8f1f1fac824aef56e08ff1f
Алексей <a.glinskiy@st-lt.ru>
2023-08-23 03:05:59 +0300

    fix jest config

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -7,6 +7,10 @@ module.exports = {
     // Stop running tests after `n` failures
     // bail: 0,
 
+    globals: {
+        __IS_DEV__: true,
+    },
+
     // The directory where Jest should store its cached dependency information
     // cacheDirectory: "C:\\Users\\Алексей\\AppData\\Local\\Temp\\jest",
 
@@ -77,9 +81,6 @@ module.exports = {
 
     // A path to a module which exports an async function that is triggered once after all test suites
     // globalTeardown: undefined,
-
-    // A set of global variables that need to be available in all test environments
-    // globals: {},
 
     // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
     // maxWorkers: "50%",
